ddbcc59c1cff1e4de60a5957de5423c3f875b3d4
marcbperez <482530+marcbperez@users.noreply.github.com>
2013-12-09 23:53:28 +0100

    Added confirmation before rebooting system and folder for portable apps.

--- firstboot.sh
+++ firstboot.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-Q="The root account will add $USER to the sudo group, update and reboot the \
-system. Are you sure? ('y' to continue) "
-
+Q="The root account will add $USER to the sudo group and update the system. \
+Are you sure? ('y' to continue) "
 read -p "$Q" -n 1 -r ; echo
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then exit 1; fi
 
 su - -c "usermod -aG sudo $USER \
-&& apt-get update && apt-get dist-upgrade && apt-get autoremove \
-&& reboot"
+&& apt-get update && apt-get dist-upgrade && apt-get autoremove"
+
+Q="Reboot the system now? ('y' to continue) "
+read -p "$Q" -n 1 -r ; echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then exit 1; fi
+
+su - -c "reboot"
