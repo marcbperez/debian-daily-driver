988715065b1d033c340cd3b8575ab15ccdc82dfb
marcbperez <482530+marcbperez@users.noreply.github.com>
2023-12-08 18:27:13 +0100

    Added GSettings configuration.

---
+++ gsettings/mousepad.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+gsettings set org.xfce.mousepad.preferences.view color-scheme "oblivion"
+gsettings set org.xfce.mousepad.preferences.view auto-indent true
+gsettings set org.xfce.mousepad.preferences.view smart-backspace true
+gsettings set org.xfce.mousepad.preferences.view highlight-current-line true
+gsettings set org.xfce.mousepad.preferences.view insert-spaces true
+gsettings set org.xfce.mousepad.preferences.view match-braces true
+gsettings set org.xfce.mousepad.preferences.view show-line-numbers true
+gsettings set org.xfce.mousepad.preferences.view show-right-margin true
+gsettings set org.xfce.mousepad.preferences.view tab-width 4
+gsettings set org.xfce.mousepad.preferences.window expand-tabs false
